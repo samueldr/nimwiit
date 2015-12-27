9bc763a3b2fdad3a0298e30dbd1998f9e9f5a5ab
Samuel Dionne-Riel <samuel@dionne-riel.com>
2015-12-26 23:07:38 -0500

    Re-factored nimwiit to handle only one wiimote for now.

--- nimwiit.lua
+++ nimwiit.lua
@@ -6,51 +6,84 @@ local inspect = require "inspect"
 require "strict"
 -- }}}
 
+local function print_help()
+	print([[
+	Usage: luajit nimwiit.lua [idx]
+	Where idx is a valid index.
+	To view the list, simply use without arguments.
+	]])
+end
+
+local args = {...}
+local idx = nil
+if #args > 0 then
+	if args[1] == "--help" or args[1] == "-h" then
+		print_help()
+		os.exit(0)
+	end
+	idx = tonumber(args[1])
+end
+
 local Monitor   = require "xwii.Monitor"
 local Interface = require "xwii.Interface"
 local utils     = require "utils"
 
 local mon = Monitor()
-
 local motes = {}
-
-print("Polling for wiimotes...")
+print("Polling for Wiimotes...")
 while true do
-	local mote = mon:poll()
-	if (mote == nil) then break end
-	table.insert(motes, mote)
-	local idx = #motes
-	if mote then
-		for _,led in pairs{1,2,3,4} do
-			mote:set_led(led, false)
-		end
-		if idx < 5 then
-			mote:set_led(idx, true)
-		else
-			mote:set_leds({false,true,false,true})
-		end
-
-		print(idx , "Device type: ", mote:get_devtype())
-		print(idx , "Device extensions: ", mote:get_extension())
-	end
+	local idx = mon:poll()
+	if (idx == nil) then break end
+	table.insert(motes, idx)
 end
-print("Found wiimotes: ",#motes)
 
--- FIXME : Add "global fd batch" thing so that they all can wait for fd.
-while #motes > 0 do
-	for i,mote in pairs(motes) do
-		local ev = mote:dispatch_event()
+if not idx or idx < 1 then
+	print("Found Wiiidxs: ".. #idxs)
+	for i,idx in pairs(idxs) do
+		print(' → #'.. i..": "..idx)
+	end
+	os.exit(0)
+end
+
+if idx > #motes then
+	print("Could not use mote "..idx..", only "..#motes.." are available.")
+	os.exit(1)
+end
+mon:destroy()
+
+if idx then
+	local mote = Interface(motes[idx])
+	for _,led in pairs{1,2,3,4} do
+		mote:set_led(led, false)
+	end
+	if idx < 5 then
+		mote:set_led(idx, true)
+	else
+		mote:set_leds({false,true,false,true})
+	end
+	print("Device type: ", mote:get_devtype())
+	print("Device extensions: ", mote:get_extension())
+
+	while true do
+	local ev = mote:dispatch_event()
 		if ev then
 			if ev.ev_name == 'KEY' then
-				print(i, inspect(ev))
+				print(inspect(ev))
+			elseif ev.ev_name == 'WATCH' then
+				print(inspect(ev))
+				-- Assumes 0 is bitmask of nothing connected.
+				-- When adding hotplugging, this should handle a disconnection more gracefully.
+				if ev.available == 0 then
+					print("Wiimote has been disconnected.")
+					os.exit(0)
+				end
+			else
+				--print(ev.ev_name)
 			end
 		else
 			--print("...")
+			-- Don't burn that CPU.
 			utils.usleep(20000)
 		end
 	end
 end
-
-mon:destroy()
-print("All done")
-
